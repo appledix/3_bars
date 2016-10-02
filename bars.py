4b05521cb1f9ff4c13d97d69613400b56cce2ca0
appledix <dxl2010@mail.ru>
2016-10-02 04:07:29 +0300

    Исправленые недочёты, добавлено описание

--- bars.py
+++ bars.py
@@ -1,30 +1,28 @@
 import json
-from math import radians, cos, sin, asin
+import sys
+from math import radians, cos, sin, asin, sqrt
+
+def read_console_args():
+    return sys.argv[1:]
+
+def alert_message(*messages):
+    for message in messages:
+        print(message)
+    return
 
 def load_data(filepath):
-    try:
-        json_file = open(filepath, 'r')
-    except:
-        while True:
-            print("Ошибка при загрузке файла.")
-            try:
-                filepath = input("Введите корректный адрес файла: \n")
-                json_file = open(filepath, 'r')
-            except Exception:
-                continue
-            else:
-                print("Файл загружен успешно.\n")
-                break
+    parsed_json = None
+    json_file = open(filepath, 'r')
     parsed_json = json.loads(json_file.read())
-    json_file.close()
+    json_file.close()                
     return parsed_json
+    
 
 def get_biggest_bar(data):
     result = "" 
-    the_biggest_bars = [["none", 0]]
+    the_biggest_bars = [[None, 0]] # Name, seats
     max_seats = 0
     current_seats = 0
-
     for bar in data:
         current_seats = bar["Cells"]["SeatsCount"]
         max_seats = the_biggest_bars[0][1]
@@ -33,17 +31,17 @@ def get_biggest_bar(data):
             if(current_seats != max_seats):
                 the_biggest_bars = []
             the_biggest_bars.append([bar_name,current_seats])
-    result += "Самый большой бар: "
+    result += "Самый большой бар Москвы: "
     for bar in the_biggest_bars:
         result += "\n -" + bar[0] + " " + str(bar[1]) + " мест"
     return result
 
+
 def get_smallest_bar(data):
     result = "" 
-    the_smallest_bars = [["none", 9999]]
+    the_smallest_bars = [[None, 9999]] # Name, seats
     min_seats = 0
     current_seats = 0
-
     for bar in data:
         current_seats = bar["Cells"]["SeatsCount"]
         min_seats = the_smallest_bars[0][1]
@@ -52,35 +50,23 @@ def get_smallest_bar(data):
             if(current_seats != min_seats):
                 the_smallest_bars = []
             the_smallest_bars.append([bar_name, current_seats])
-    result += "Самый маленький бар: "
+    result += "Самый маленький бар Москвы: "
     for bar in the_smallest_bars:
         result += "\n -" + bar[0] + " " + str(bar[1]) + " мест"
     return result
 
-def calculate_distance(lat1, lon1, lat2, lon2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(a**0.5) 
-    km = 6367 * c
-    return km
+def get_closest_bar(data, latitude, longitude):
+    if not latitude_is_valid(latitude) or not longitude_is_valid(longitude):
+        raise Exception
 
-def get_closest_bar(data, longitude, latitude):
     min_distance = 384401
-    nearest_bar = "none"
+    nearest_bar = None
     current_distance = 0
     for bar in data:
-        bar_pos_lat = bar["Cells"]["geoData"]["coordinates"][0]
-        bar_pos_lon = bar["Cells"]["geoData"]["coordinates"][1]
-        current_distance = calculate_distance(bar_pos_lon, bar_pos_lat, \
-            longitude, latitude)
+        bar_pos_lon = bar["Cells"]["geoData"]["coordinates"][0]
+        bar_pos_lat = bar["Cells"]["geoData"]["coordinates"][1]
+        current_distance = calculate_distance(bar_pos_lat, bar_pos_lon , \
+            latitude, longitude)
         if(current_distance < min_distance):
             min_distance = current_distance
             nearest_bar = bar["Cells"]["Name"] + \
@@ -88,23 +74,81 @@ def get_closest_bar(data, longitude, latitude):
             "\nРасстояние :" + str(round(current_distance, 3)) + " км"
     return nearest_bar
 
-def get_user_coordinates():
-    print("\nВвод gps-координат.")
-    while True:
-        try:
-            input_coords = \
-            input("Введите gps-координаты в формате '[широта], [долгота]': \n").split(",")
-            result_longitude = float(input_coords[0].strip())
-            result_latitude = float(input_coords[1].strip())
-        except Exception:
-            print ("Некорректный ввод, попробуйте ещё раз.")
-        else:
-            break
-    return result_longitude,result_latitude
+def calculate_distance(start_latitude, start_longitude, end_latitude, end_longitude):
+    if not latitude_is_valid(start_latitude) or \
+    not latitude_is_valid(end_latitude) or \
+    not longitude_is_valid(start_longitude) or \
+    not longitude_is_valid(end_longitude):
+        raise Exception
 
-filepath = 'bars.json'
-data = load_data(filepath)
-print(get_biggest_bar(data))
-print(get_smallest_bar(data))
-longitude, latitude = get_user_coordinates()
-print("Ближайший бар: ", get_closest_bar(data, longitude, latitude))
+    earth_radius = 6367
+    start_longitude, start_latitude, end_longitude, end_latitude = \
+    map(radians, [start_longitude, start_latitude, \
+        end_longitude, end_latitude]) # convert decimal degrees to radians 
+    longitude_difference = end_longitude - start_longitude 
+    latitude_difference = end_latitude - start_latitude 
+    haversine_formula  = 2 * asin(sqrt(sin(latitude_difference / 2)**2 + \
+        cos(start_latitude) * cos(end_latitude) * sin(longitude_difference / 2)**2))
+    return haversine_formula * earth_radius
+
+def latitude_is_valid(latitude):
+    if (type(latitude) == float) and (latitude >= -90) and (latitude <= 90):
+        return True
+    else:
+        return False
+
+def longitude_is_valid(longitude):
+    if (type(longitude) == float) and (longitude >= -180) and (longitude <= 180):
+        return True
+    else:
+        return False
+
+def get_user_coordinates():
+    longitude = None
+    latitude = None
+    alert_message("Введите широту и долготу через пробел: ")
+    coordinates = input().split(" ")
+    latitude = float(coordinates[0])
+    longitude = float(coordinates[1])
+    if not latitude_is_valid(latitude) or not longitude_is_valid(longitude):
+        raise Exception
+    return latitude, longitude    
+
+
+if __name__ == '__main__':
+    parameters = read_console_args()
+    try:
+        if len(parameters) != 2:
+            raise Exception
+        filepath = parameters[0]
+        argument = parameters[1]
+        data = load_data(filepath)
+    except Exception:
+        alert_message("""Для работы скрипта необходимы два аргумента, переданные через пробел:
+1) Валидный адрес JSON файла с данными о барах Москвы для анализа. 
+2) Параметр '-bars' для поиска самого большого и маленького баров 
+ или '-search' для поиска ближайшего бара по вводимым координатам.""")
+    else:
+        if argument == "-bars":
+            the_smallest_bars = get_smallest_bar(data)
+            the_biggest_bars = get_biggest_bar(data)
+            alert_message(the_smallest_bars, the_biggest_bars)
+
+        elif argument == "-search":
+            alert_message("Поиск ближайшего бара. Введите текущие координаты.")
+            while True:
+                try:
+                    latitude, longitude = get_user_coordinates()
+                except Exception:
+                    alert_message("Введите корректные данные!")
+                else:
+                    break
+            try:
+                closest_bar = get_closest_bar(data, latitude, longitude)
+            except Exception:
+                alert_message("Произошла ошибка при попытке поиска ближайшего бара.")
+            else:
+                alert_message("\nБлижайший бар: ", closest_bar)
+        
+        else:
+            alert_message("Скрипт работает только с аргументами '-bars' или '-search'.")
